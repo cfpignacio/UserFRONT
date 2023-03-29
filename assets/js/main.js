c7d0589a97e7cbdadb4765e1137b08014e8293a4
Ignacio Camillucci <ignacio.camillucci@gmail.com>
2023-03-29 20:40:39 -0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+ async function getUser(){
+    var apiUser =  await fetch('https://randomuser.me/api/')
+    var dataJson =  await apiUser.json()
+    var user = dataJson.results[0]
+
+    var fullName = `${user.name.first} ${user.name.last}`
+    var email = user.email
+    
+    const userInfo = `
+    <h2>${user.name.first} ${user.name.last}</h2>
+    <img src="${user.picture.large}">
+    <p>phone: ${user.phone}</p>
+    <p>email: ${user.email}</p>
+    `
+
+
+    document.getElementById("user").innerHTML = userInfo
+
+}
